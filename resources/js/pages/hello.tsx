ea8f65a4e35b47b66a5fcccc0840735d67fb6200
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-08-11 14:41:16 +0900

    feat: simple inertia react

---
+++ resources/js/pages/hello.tsx
@@ -0,0 +1,54 @@
+import { Head } from '@inertiajs/react';
+import { JSX, useState } from 'react';
+
+export default function Hello(): JSX.Element {
+    const [inputValue, setInputVlaue] = useState('');
+    const [message, setMessage] = useState('Hello!');
+
+    // フォーム送信時の処理
+    const handleSubmit = (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        setMessage('こんにちは, ' + inputValue + 'さん！');
+    };
+    return (
+        <div style={{ padding: '20px' }}>
+            <Head title="Hello" />
+            <h1
+                style={{
+                    fontSize: '30px',
+                    paddingBottom: '10px',
+                }}
+                className="pb-5 text-3xl"
+            >
+                Hello Page
+            </h1>
+            <p
+                style={{
+                    fontSize: '20px',
+                }}
+            >
+                {message}
+            </p>
+            <form onSubmit={handleSubmit} style={{ marginTop: '20px' }}>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputVlaue(e.target.value)}
+                    placeholder="名前を入力"
+                    style={{ padding: '8px', fontSize: '16px', width: '300px' }}
+                />
+                <button
+                    type="submit"
+                    style={{
+                        padding: '8px 16px',
+                        marginLeft: '10px',
+                        background: 'blue',
+                        color: 'white',
+                    }}
+                >
+                    送信
+                </button>
+            </form>
+        </div>
+    );
+}
