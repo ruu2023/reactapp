85c24cf1e29e23413f8797374ba55826831988f9
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-08-16 18:05:53 +0900

    style: tab size

--- resources/js/pages/hello.tsx
+++ resources/js/pages/hello.tsx
@@ -3,64 +3,64 @@ import axios from 'axios';
 import { JSX, useState } from 'react';
 
 export default function Hello(): JSX.Element {
-    const [inputValue, setInputVlaue] = useState('');
-    // const [message, setMessage] = useState('Hello!');
-    const [serverMessage, setServerMessage] = useState('Hello!');
+  const [inputValue, setInputVlaue] = useState('');
+  // const [message, setMessage] = useState('Hello!');
+  const [serverMessage, setServerMessage] = useState('Hello!');
 
-    // フォーム送信時の処理
-    const handleSubmit = (e: { preventDefault: () => void }) => {
-        e.preventDefault();
-        // setMessage('こんにちは, ' + inputValue + 'さん！');
-        axios
-            .post('/update-message', { number: inputValue })
-            .then((response) => {
-                console.log(response);
-                setServerMessage(response.data.message);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    };
-    return (
-        <div style={{ padding: '20px' }}>
-            <Head title="Hello" />
-            <h1
-                style={{
-                    fontSize: '30px',
-                    paddingBottom: '10px',
-                }}
-                className="pb-5 text-3xl"
-            >
-                {/* Hello Page */}
-                welcome page
-            </h1>
-            <p
-                style={{
-                    fontSize: '20px',
-                }}
-            >
-                {serverMessage}
-            </p>
-            <form onSubmit={handleSubmit} style={{ marginTop: '20px' }}>
-                <input
-                    type="text"
-                    value={inputValue}
-                    onChange={(e) => setInputVlaue(e.target.value)}
-                    placeholder="名前を入力"
-                    style={{ padding: '8px', fontSize: '16px', width: '300px' }}
-                />
-                <button
-                    type="submit"
-                    style={{
-                        padding: '8px 16px',
-                        marginLeft: '10px',
-                        background: 'blue',
-                        color: 'white',
-                    }}
-                >
-                    送信
-                </button>
-            </form>
-        </div>
-    );
+  // フォーム送信時の処理
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    // setMessage('こんにちは, ' + inputValue + 'さん！');
+    axios
+      .post('/update-message', { number: inputValue })
+      .then((response) => {
+        console.log(response);
+        setServerMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  return (
+    <div style={{ padding: '20px' }}>
+      <Head title="Hello" />
+      <h1
+        style={{
+          fontSize: '30px',
+          paddingBottom: '10px',
+        }}
+        className="pb-5 text-3xl"
+      >
+        {/* Hello Page */}
+        welcome page
+      </h1>
+      <p
+        style={{
+          fontSize: '20px',
+        }}
+      >
+        {serverMessage}
+      </p>
+      <form onSubmit={handleSubmit} style={{ marginTop: '20px' }}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputVlaue(e.target.value)}
+          placeholder="名前を入力"
+          style={{ padding: '8px', fontSize: '16px', width: '300px' }}
+        />
+        <button
+          type="submit"
+          style={{
+            padding: '8px 16px',
+            marginLeft: '10px',
+            background: 'blue',
+            color: 'white',
+          }}
+        >
+          送信
+        </button>
+      </form>
+    </div>
+  );
 }
